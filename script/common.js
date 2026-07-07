382a258734b2e844c5c1a0b438ed2bc656a5ee7d
yoonhm <me459766@gmail.com>
2026-07-07 14:53:34 +0900

    header/footer include

--- script/common.js
+++ script/common.js
@@ -1,19 +1,24 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const elements = document.querySelectorAll('[data-include]');
+
     for (const el of elements) {
         const file = el.getAttribute('data-include');
-        if (file) {
-            try {
-                const res = await fetch(file);
-                if (res.ok) {
-                    el.innerHTML = await res.text();
-                } else {
-                    console.error('헤더 파일 불러오기 실패:', res.status);
-                }
-            } catch (err) {
-                console.error('fetch 에러:', err);
+
+        if (!file) return;
+
+        try {
+            const res = await fetch(file);
+
+            if (res.ok) {
+                el.innerHTML = await res.text();
+            } else {
+                console.error('공통 파일 불러오기 실패:', file, res.status);
             }
+        } catch (err) {
+            console.error('fetch 에러:', file, err);
         }
     }
-});
 
+    // header/footer 로드 후 실행할 공통 함수
+    setActiveMenu();
+});
